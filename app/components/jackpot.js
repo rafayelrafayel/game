484e26df66f92c6b89acc97a0f487bd7b7b74070
minas <minas@freedom-dev.com>
2016-07-02 16:35:19 +0400

    - Added sounds in jackpot.

--- app/components/jackpot.js
+++ app/components/jackpot.js
@@ -82,41 +82,53 @@ System.register(['@angular/core', '../../app/services/JackpotSvc'], function(exp
                     this.getHighStakeInfo();
                     jQuery(document).ready(function () {
                         (function ($) {
-                            var h = $("div.select-winner").height();
-                            var el = $("div.select-winner");
-                            var duration = 7000;
-                            var playes_count = el.children().length;
-                            var step = el.children().height();
-                            $("div.select-winner > *").clone().appendTo($("div.select-winner"));
-                            var index = 0;
-                            var duration = 50;
-                            var top = -$("div.select-winner").height() + 180;
-                            function loop(duration, top) {
-                                $("div.select-winner").css('top', top);
-                                for (var i = 0; i <= 30; i++) {
-                                    if (i == 30) {
-                                        var a = (Math.floor(Math.random() * (playes_count - 1)) - 1) + playes_count;
-                                        duration = 2000;
-                                        top = -step * a;
-                                        setTimeout(function () {
-                                            $("div.winner").fadeIn(300);
-                                            $("div.winner .circle-winner").css("background-image", "url(" + el.children().eq(a + 1).find("img").attr("src") + ")");
-                                            $("#enter-jackpot").prop('disabled', false);
-                                        }, 30 * 50 * 2 + 3500);
+                            var counter = 0;
+                            var top = "";
+                            var sample = new Audio("/sounds/slot_jackpot.mp3");
+                            var w = new Audio("/sounds/bell_ring.mp3");
+                            function slot() {
+                                var duration = 100;
+                                $("div.select-winner").animate({
+                                    top: -$("div.select-winner").height(),
+                                },duration, function() {
+                                    $(this).css('top', 0);
+                                    counter++;
+                                    if (counter < 10 * 7) {
+                                        slot();
+                                    } else {
+                                        $("div.select-winner").css('top', -$("div.select-winner").height() + 3 * 90);
+                                        var max = $("div.select-winner").children('div').length/2 - 1;
+                                        var min = 0;
+                                        var index = Math.floor(Math.random() * (max - min + 1)) + min;
+                                        duration = 2500;
+                                        top = -90 * (index+1);
+                                        $("div.select-winner").animate({top: top}, duration, 'easeOutExpo', function() {
+                                            var winner = $("div.participants").children('div').eq(index);
+                                            $("div.winner .circle-winner").css("background-image", "url("+winner.find("img").attr("src")+")");
+                                            $("div.win-info span.name").text(winner.find("span.name").text());
+                                            $("div.winner").delay(1000).fadeIn(300, function(){
+                                                w.play();
+                                                $("div.select-winner > div").slice(-max-1).remove();
+                                                $("div.select-winner").css('top',0);
+                                                $("#enter-jackpot").prop('disabled', false);
+                                                counter = 0;
+                                            });
+                                        });
                                     }
-                                    $("div.select-winner").animate({ top: top }, duration, 'easeOutExpo');
-                                    if (i != 30) {
-                                        $("div.select-winner").animate({ top: 0 }, duration);
-                                    }
-                                }
+                                });
+                            }
+                            function enterJackpot() {
+                                $("div.select-winner > *").clone().appendTo($("div.select-winner"));
+                                sample.play();
+                                slot();
+                                $("div.winner").hide();
+                                $(".circle-winner").css("visibility", "visible");
+                                $("div.circle-winner").show();
                             }
                             $("#enter-jackpot").click(function (e) {
                                 $(this).prop('disabled', true);
                                 e.preventDefault();
-                                $("div.winner").hide();
-                                $(".circle-winner").css("visibility", "visible");
-                                $("div.circle-winner").show();
-                                loop(duration, top);
+                                enterJackpot();
                             });
                         })(jQuery);
                     });
