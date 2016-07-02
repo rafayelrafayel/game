e94a93da4c19ca2c44e20b5d0a97c6c76345fc16
minas <minas@freedom-dev.com>
2016-07-02 17:16:22 +0400

    - Jackpot js into jackpot.ts

--- app/components/jackpot.js
+++ app/components/jackpot.js
@@ -90,26 +90,27 @@ System.register(['@angular/core', '../../app/services/JackpotSvc'], function(exp
                                 var duration = 100;
                                 $("div.select-winner").animate({
                                     top: -$("div.select-winner").height(),
-                                },duration, function() {
+                                }, duration, function () {
                                     $(this).css('top', 0);
                                     counter++;
                                     if (counter < 10 * 7) {
                                         slot();
-                                    } else {
+                                    }
+                                    else {
                                         $("div.select-winner").css('top', -$("div.select-winner").height() + 3 * 90);
-                                        var max = $("div.select-winner").children('div').length/2 - 1;
+                                        var max = $("div.select-winner").children('div').length / 2 - 1;
                                         var min = 0;
                                         var index = Math.floor(Math.random() * (max - min + 1)) + min;
                                         duration = 2500;
-                                        top = -90 * (index+1);
-                                        $("div.select-winner").animate({top: top}, duration, 'easeOutExpo', function() {
+                                        top = (-90 * (index + 1)).toString();
+                                        $("div.select-winner").animate({ top: top }, duration, 'easeOutExpo', function () {
                                             var winner = $("div.participants").children('div').eq(index);
-                                            $("div.winner .circle-winner").css("background-image", "url("+winner.find("img").attr("src")+")");
+                                            $("div.winner .circle-winner").css("background-image", "url(" + winner.find("img").attr("src") + ")");
                                             $("div.win-info span.name").text(winner.find("span.name").text());
-                                            $("div.winner").delay(1000).fadeIn(300, function(){
+                                            $("div.winner").delay(1000).fadeIn(300, function () {
                                                 w.play();
-                                                $("div.select-winner > div").slice(-max-1).remove();
-                                                $("div.select-winner").css('top',0);
+                                                $("div.select-winner > div").slice(-max + 1).remove();
+                                                $("div.select-winner").css('top', 0);
                                                 $("#enter-jackpot").prop('disabled', false);
                                                 counter = 0;
                                             });
